ca5db8e3b9b056f1f3cc845cdfdbc9b3d9909bc9
pphatdev <leatsophat02@gmail.com>
2024-02-22 17:20:35 +0700

    22022024_0520

--- src/js/index.js
+++ src/js/index.js
@@ -1,72 +1,151 @@
-import { loadFull } from "tsparticles";
+import { tsParticles } from "@tsparticles/engine";
 import { logo  } from './helpers/assets'
 
-(async() => {
-    await loadFull(tsParticles);
-    await tsParticles.load({
-        id: "tsparticles",
-        options: {
-            background: {
-                color: "#000",
-                image: logo,
-                repeat: "no-repeat",
-                size: "20%",
-                position: "50% 50%"
-            },
-            interactivity: {
-                events: {
-                    onClick: {
-                        enable: true,
-                        mode: "repulse"
-                    },
-                    onHover: {
-                        enable: true,
-                        mode: "bubble"
-                    }
-                },
-                modes: {
-                bubble: {
-                    distance: 200,
-                    duration: 2,
-                    opacity: 0,
-                    size: 0,
-                    speed: 3
-                },
-                repulse: {
-                    distance: 400,
-                    duration: 0.4
-                }
-                }
-            },
-            particles: {
-                color: { value: "#ffffff" },
-                move: {
-                    direction: "none",
+await tsParticles.load({
+    id: "tsparticles",
+    options: {
+        background: {
+            color: "#0000",
+            image: logo,
+            repeat: "no-repeat",
+            size: "20%",
+            position: "50% 50%"
+        },
+        interactivity: {
+            events: {
+                onClick: {
                     enable: true,
-                    outModes: "out",
-                    random: true,
-                    speed: 0.3
+                    mode: "repulse"
                 },
-                number: {
-                    density: {
-                        enable: true
-                    },
-                    value: 600
-                },
-                opacity: {
-                    animation: {
-                        enable: true,
-                        speed: 5
-                    },
-                    value: { min: 0.3, max: 0.6 }
-                },
-                shape: {
-                    type: "circle"
-                },
-                size: {
-                    value: 1
+                onHover: {
+                    enable: true,
+                    mode: "bubble"
                 }
+            },
+            modes: {
+            bubble: {
+                distance: 200,
+                duration: 2,
+                opacity: 0,
+                size: 0,
+                speed: 3
+            },
+            repulse: {
+                distance: 400,
+                duration: 0.4
+            }
+            }
+        },
+        particles: {
+            color: { value: "#ffffff" },
+            move: {
+                direction: "none",
+                enable: true,
+                outModes: "out",
+                random: true,
+                speed: 0.3
+            },
+            number: {
+                density: {
+                    enable: true
+                },
+                value: 600
+            },
+            opacity: {
+                animation: {
+                    enable: true,
+                    speed: 5
+                },
+                value: { min: 0.3, max: 0.6 }
+            },
+            shape: {
+                type: "circle"
+            },
+            size: {
+                value: 1
             }
         }
-    });
-})()
+    }
+})
+
+// tsParticles.setOnClickHandler((event, particles) => {
+//     /* custom on click handler */
+// });
+// const particles = tsParticles.domItem(0);
+
+// particles.play();
+// // pause will stop the animations
+// particles.pause();
+
+
+
+// (async() => {
+
+//     const loader = await loadFull();
+//     await loader.load({
+//         id: "tsparticles",
+//         options: {
+//             background: {
+//                 color: "#000",
+//                 image: logo,
+//                 repeat: "no-repeat",
+//                 size: "20%",
+//                 position: "50% 50%"
+//             },
+//             interactivity: {
+//                 events: {
+//                     onClick: {
+//                         enable: true,
+//                         mode: "repulse"
+//                     },
+//                     onHover: {
+//                         enable: true,
+//                         mode: "bubble"
+//                     }
+//                 },
+//                 modes: {
+//                 bubble: {
+//                     distance: 200,
+//                     duration: 2,
+//                     opacity: 0,
+//                     size: 0,
+//                     speed: 3
+//                 },
+//                 repulse: {
+//                     distance: 400,
+//                     duration: 0.4
+//                 }
+//                 }
+//             },
+//             particles: {
+//                 color: { value: "#ffffff" },
+//                 move: {
+//                     direction: "none",
+//                     enable: true,
+//                     outModes: "out",
+//                     random: true,
+//                     speed: 0.3
+//                 },
+//                 number: {
+//                     density: {
+//                         enable: true
+//                     },
+//                     value: 600
+//                 },
+//                 opacity: {
+//                     animation: {
+//                         enable: true,
+//                         speed: 5
+//                     },
+//                     value: { min: 0.3, max: 0.6 }
+//                 },
+//                 shape: {
+//                     type: "circle"
+//                 },
+//                 size: {
+//                     value: 1
+//                 }
+//             }
+//         }
+//     });
+// })()
