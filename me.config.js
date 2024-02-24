7829f84948c600c7611fb08ea2c15ce4b0eef3ce
pphatdev <leatsophat02@gmail.com>
2024-02-24 23:51:02 +0700

    Update Description

--- me.config.js
+++ me.config.js
@@ -1,7 +1,7 @@
 export const me = {
     fullName: "Leat Sophat",
     shortName:  "Sophat",
-    description: "This Website is showing about Mr.Leat Sophat",
+    description: "Welcome to my portfolio - Thoughts from a wandering mind. I am the senior front-end developer of TurboTech, and an UI/UX designer. In my free time, I like design side projects and developing them.",
     start_url: "/",
     background_color: "#ffffff",
     theme_color: "#ffffff",
