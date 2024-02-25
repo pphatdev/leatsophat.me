014f366e88d2cd2dd285155501a4ab6dc9644852
pphatdev <leatsophat02@gmail.com>
2024-02-25 14:18:20 +0700

    25022024_0202

--- pages.config.js
+++ pages.config.js
@@ -2,5 +2,6 @@ export const pages = [
     { name: "Home", route: "home" },
     { name: "Project", route: "project" },
     { name: "Contact", route: "contact" },
+    { name: "Blogs", route: "https://blog.leatsophat.me/" },
     { name: "About Me", route: "about-me" },
 ]
