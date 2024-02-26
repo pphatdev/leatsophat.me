bc2bea965cc4942f1e05ac7d4a37ddf22bbdc360
pphatdev <leatsophat02@gmail.com>
2024-02-26 18:20:53 +0700

    Move Profile Hero to Component

---
+++ src/components/hero-profile.js
@@ -0,0 +1,4 @@
+// Create Image for Hero Sections of Home page
+export const showProfile = () => {
+    return `<img src="${fav512.replace("dist",'.')}" loading="lazy" class="w-36 rounded-full sm:rounded-2xl h-36 sm:w-44 md:w-64 sm:h-44 md:h-64 my-7" alt="Leat Sophat">`
+}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,9 @@
+import "../helpers/assets";
 import { Header } from "../components/header.js";
-import { fav512 } from "../helpers/assets";
+import { showProfile } from "../components/hero-profile.js";
 import { codingWith, frameworksWith, onlineToolsWith, stylingWith } from "../helpers/candowith";
 import { sidebar, toggleTheme } from "../helpers/header.js";
 
-export const showProfile = () => {
-    return `<img src="${fav512.replace("dist",'.')}" loading="lazy" class="w-36 rounded-full sm:rounded-2xl h-36 sm:w-44 md:w-64 sm:h-44 md:h-64 my-7" alt="Leat Sophat">`
-}
 
 
 document.querySelector('[data-image]').innerHTML = showProfile()
