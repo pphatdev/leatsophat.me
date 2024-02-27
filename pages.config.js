27ed802e2bbb19ef5d482f75532dfd010e901619
pphatdev <leatsophat02@gmail.com>
2024-02-27 09:50:49 +0700

    Create Sample Page and Remove Old Page

--- pages.config.js
+++ pages.config.js
@@ -1,6 +1,6 @@
 export const pages = [
     { name: "Home", route: "home" },
-    { name: "Project", route: "project" },
-    { name: "Contact", route: "contact" },
-    { name: "About Me", route: "about-me" },
+    // { name: "Project", route: "project" },
+    // { name: "Contact", route: "contact" },
+    // { name: "About Me", route: "about-me" },
 ]
