2b42b20c8d87db890e0f78b9348ea2ab8f0fbf39
pphatdev <leatsophat02@gmail.com>
2024-03-03 10:13:45 +0700

    Aboutme

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,21 +1,22 @@
 import { timelines } from "../data/timeline"
+import { minify } from "../helpers/stringMinify"
 
 const list = (data) => {
     return(data.map(item => {
-        return(`
-            <li>
-                <h2 class="py-3 text-xl font-bold">${item?.title}</h2>
-                <ul class="list-inside list-item">
+        return(String(
+            `<li>
+                <h2 class="py-3 dark:text-primary text-xl font-bold">${item?.title}</h2>
+                <ul class="list-inside list-item dark:text-white/60">
                     ${item?.list.map(list => {
                         return(`<li class="list-disc">${list?.title}</li>`)
                     })}
                 </ul>
-            </li>
-        `)
+            </li>`
+        ))
     }))
 }
 
 export const timeline = () => {
     const tartget = document.querySelector('#summary')
-    tartget.innerHTML = list(timelines)
+    tartget.innerHTML = minify(list(timelines)).replaceAll(/\>\,\</g, "><")
 }
