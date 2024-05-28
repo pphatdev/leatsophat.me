81fd5cb812eae29b55ec327adc2171a1d1c46aa0
pphatdev <leatsophat02@gmail.com>
2024-05-28 22:07:50 +0700

    update Hero

--- me.config.js
+++ me.config.js
@@ -1,7 +1,8 @@
 export const me = {
-    fullName: "Leat Sophat",
-    shortName:  "Sophat",
-    description: "Welcome to my portfolio - Thoughts from a wandering mind. I am the senior front-end developer of TurboTech, and an UI/UX designer. In my free time, I like design side projects and developing them.",
+    fullName: "Leat Sophat | លាត សុផាត",
+    shortName:  "Sophat | សុផាត",
+    description: `Welcome to my portfolio - Thoughts from a wandering mind. I am the senior front-end developer of TurboTech, and an UI/UX designer. In my free time, I like design side projects and developing them.
+    | សូមស្វាគមន៍មកកាន់ Portfolio របស់ខ្ញុំ សព្វថ្ងៃខ្ញុំជាអ្នកអភិវឌ្ឍន៍ផ្នែកខាងមុខជាន់ខ្ពស់នៃក្រុមហ៊ុន TurboTech និងជាអ្នករចនា UI/UX ។ ពេលទំនេរ ខ្ញុំចូលចិត្តគម្រោងផ្នែកខាងរចនា និងអភិវឌ្ឍវា។`,
     start_url: "/",
     background_color: "#ffffff",
     theme_color: "#ffffff",
